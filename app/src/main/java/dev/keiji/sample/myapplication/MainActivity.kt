72e27a7102e24b705144a9b64903d3e713ec469d
LAPTOP-OB2G7TH9\Owner <bkg98970@gmail.com>
2021-08-07 17:03:13 +0900

    P.59　リスト04.3がエラーだよ

--- app/src/main/java/dev/keiji/sample/myapplication/MainActivity.kt
+++ app/src/main/java/dev/keiji/sample/myapplication/MainActivity.kt
@@ -2,21 +2,19 @@ package dev.keiji.sample.myapplication
 
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import io.keiji.sample.mastodonclient.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView( R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            val fragment = MainFragment()
-            supportFragmentManager.beginTransAction()
-                .add(
-                    R.id.fragment_container,
-                    fragment,
-                    MainFragment::class.java.simpleName
-                )
-                .commit()
-        }
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this.parent
+                    R.layout.activity_main
+        )
+        binding.textview.text = "Hello DataBinding!"
     }
 }
+
