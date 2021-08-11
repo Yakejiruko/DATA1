8d7de4850d96dc5aac970716796e6b0d97c92740
LAPTOP-OB2G7TH9\Owner <bkg98970@gmail.com>
2021-08-11 21:55:13 +0900

    P85の作業まで進めたけどRUNできないよ

--- app/src/main/java/dev/keiji/sample/myapplication/MainFragment.kt
+++ app/src/main/java/dev/keiji/sample/myapplication/MainFragment.kt
@@ -9,10 +9,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.squareup.moshi.Json
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dev.keiji.sample.myapplication.R
 import dev.keiji.sample.myapplication.databinding.FragmentMainBinding
 import retrofit2.Retrofit
 import android.util.Log
+import dev.keiji.sample.mastodonclient.Toot
 import dev.keiji.sample.myapplication.MastodonApi
 
 class MainFragment : Fragment(R.layout.fragment_main) {
@@ -21,9 +24,14 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         private val TAG = MainFragment::class.java.simpleName
         private const val API_BASE_URL = "http://androidbook2020.keiji.io"
     }
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
     private val retrofit = Retrofit.Builder()
 
         .baseUrl(API_BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
     private val api = retrofit.create(MastodonApi::class.java)
 
@@ -35,11 +43,8 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             binding?.button?.text="clicked"
 
             CoroutineScope(Dispatchers.IO).launch {
-                val response = api.fetchPublicTimeline().string()
-                Log.d(TAG, response)
-                withContext(Dispatchers.Main) {
-                    binding?.button?.text = response
-                }
+                val tootList = api.fetchPublicTimeline()
+                showTootList(tootList)
             }
 
         }
@@ -49,4 +54,15 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         super.onDestroyView()
         binding?.unbind()
     }
+
+    private suspend fun showTootList(
+        tootList: List<Toot>
+    ) = withContext(Dispatchers.Main) {
+        val binding = binding ?: return@withContext
+        val accountNameList = tootList.map {it.account.displayName}
+        binding.button.text = accountNameList.joinToString("/n")
+    }
+
+    }
+    )
 }
