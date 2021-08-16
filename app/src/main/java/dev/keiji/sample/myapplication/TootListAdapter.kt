223a850b20e3908d7ff757c5b456f0b00eddfe33
LAPTOP-OB2G7TH9\Owner <bkg98970@gmail.com>
2021-08-16 15:06:14 +0900

    P89の作業まで進めたけどエラーになるよ

---
+++ app/src/main/java/dev/keiji/sample/myapplication/TootListAdapter.kt
@@ -0,0 +1,43 @@
+package dev.keiji.sample.myapplication
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import dev.keiji.sample.mastodonclient.Toot
+import dev.keiji.sample.mastodonclient.databinding.ListItemTootBinding
+import dev.keiji.sample.myapplication.databinding.ListItemTootBinding
+
+class TootListAdapter(
+    private val layoutInflater: LayoutInflater,
+    private val tootList: ArrayList<Toot>
+) : RecyclerView.Adapter<TootListAdapter.ViewHolder>() {
+
+    override fun getItemCount() = tootList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = DataBindingUtil.inflate<ListItemTootBinding>(
+            layoutInflater,
+            R.layout.list_item_toot,
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: ViewHolder,
+        position: Int
+    ) {
+        holder.bind(tootList[position])
+    }
+
+    class ViewHolder(
+        private val binding: ListItemTootBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(toot: Toot) {
+            binding.userName.text = toot.account.username
+            binding.content.text = toot.content
+        }
+    }
+}
