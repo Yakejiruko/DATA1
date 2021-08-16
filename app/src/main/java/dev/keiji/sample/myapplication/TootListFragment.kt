4b609aeaa5c1abc9bfadeb5337c336a6a37b7ff6
LAPTOP-OB2G7TH9\Owner <bkg98970@gmail.com>
2021-08-16 16:09:28 +0900

    P93の作業まで進めたけどエラーになるよ

---
+++ app/src/main/java/dev/keiji/sample/myapplication/TootListFragment.kt
@@ -0,0 +1,84 @@
+package dev.keiji.sample.myapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dev.keiji.sample.myapplication.databinding.FragmentMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import dev.keiji.sample.mastodonclient.Toot
+
+import com.squareup.moshi.Json
+import dev.keiji.sample.myapplication.R
+import android.util.Log
+import com.squareup.moshi.Moshi
+import dev.keiji.sample.myapplication.MastodonApi
+import dev.keiji.sample.myapplication.databinding.FragmentTootListBinding
+
+
+class TootListFragment : Fragment(R.layout.fragment_toot_list) {
+
+    companion object {
+        val TAG = TootListFragment::class.java.simpleName
+        private const val API_BASE_URL = "https://androidbook2020.keiji.dev"
+    }
+
+    private var binding: FragmentTootListBinding? = null
+
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(API_BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+    private val api = retrofit.create(MastodonApi::class.java)
+
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    private lateinit var adapter: TootListAdapter
+    private lateinit var lauoutManager: LinearLayoutManager
+
+    private val tootList = ArrayList<Toot>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter = TootListAdapter(layoutInflater, tootList)
+        layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        val bindingData: FragmentTootListBinding? = DataBindingUtil.bind(view)
+        binding = bindingData ?: return
+
+        bindingData.recyclerView.also {
+            it.layoutManager = lauoutManager
+            it.adapter = adapter
+        }
+
+        coroutineScope.launch {
+            val tootList = api.fetchPublicTimeline()
+            tootList.addAll(tootList)
+            reloadTootList()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding?.unbind()
+    }
+
+    private suspend fun reloadTootList() = withContext(Dispatchers.Main) {
+        adapter.notifyDataSetChanged()
+    }
+}
