04b8d2cb7376fcb70b8362a5f52717547a8598eb
LAPTOP-OB2G7TH9\Owner <bkg98970@gmail.com>
2021-08-21 11:39:12 +0900

    P108まで進めたけどエラーだよ

--- app/src/main/java/dev/keiji/sample/myapplication/Toot.kt
+++ app/src/main/java/dev/keiji/sample/myapplication/Toot.kt
@@ -1,12 +1,17 @@
 package dev.keiji.sample.mastodonclient
 
 import com.squareup.moshi.Json
+import dev.keiji.sample.myapplication.Media
 
 data class Toot (
     val id: String,
-    @Json(name = "created_at") val createdAt: String ,
+    @Json(name = "created_at") val createdAt: String,
     val sensitive: Boolean,
     val url: String,
+    @Json(name = "media_attachments") val mediaAttachments: List<Media>,
     val content: String,
     val account: Account
-)
+) {
+    val topMedia: Media?
+        get() = mediaAttachments.firstOrNull()
+}
--- app/src/main/java/dev/keiji/sample/myapplication/TootListFragment.kt
+++ app/src/main/java/dev/keiji/sample/myapplication/TootListFragment.kt
@@ -5,15 +5,13 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dev.keiji.sample.myapplication.databinding.FragmentMainBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.atomic.AtomicBoolean
 import dev.keiji.sample.mastodonclient.Toot
 
 import com.squareup.moshi.Json
@@ -21,9 +19,36 @@ import dev.keiji.sample.myapplication.R
 import android.util.Log
 import dev.keiji.sample.myapplication.MastodonApi
 import dev.keiji.sample.myapplication.databinding.FragmentTootListBinding
+import kotlinx.coroutines.*
 
 
 class TootListFragment : Fragment(R.layout.fragment_toot_list) {
+
+    private lateinit var layoutManager: LinearLayoutManager
+
+    private var isLoading = AtomicBoolean()
+    private var hasNext = AtomicBoolean() . apply{ set(true) }
+
+    private val loadNextScrollListner = object : RecyclerView.
+    OnScrollListener() {
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            if (isLoading.get() || !hasNext.get()) {
+                return
+            }
+
+            val visibleItemCount = recyclerView.childCount
+            val totalItemCount = layoutManager.itemCount
+            val firstVisibleItemPosition = layoutManager.
+                    findFirstVisibleItemPosition()
+
+            if ((totalItemCount - visibleItemCount) <=
+                    firstVisibleItemPosition) {
+                loadNext()
+            }
+        }
+    }
 
     companion object {
         val TAG = TootListFragment::class.java.simpleName
@@ -63,18 +88,32 @@ class TootListFragment : Fragment(R.layout.fragment_toot_list) {
         bindingData.recyclerView.also {
             it.layoutManager = layoutManager
             it.adapter = adapter
+            it.addOnScrollListener(loadNextScrollListner)
         }
 
-        coroutineScope.launch {
-            val tootListResponse = api.fetchPublicTimeline()
-            tootList.addAll(tootListResponse)
-            reloadTootList()
-        }
+        laodNext()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding?.unbind()
+    }
+
+    private fun loadNext() {
+        coroutineScope.launch{
+            isLoading.set(true)
+
+            val tootListResponse = api.fetchPublicTimeline(
+                maxId = tootList.lastOrNull() ?.id,
+                onlyMedia = true
+            )
+
+            tootList.addAll(tootListResponse.filter { !it.sensitive})
+            reloadTootList()
+
+            isLoading.set(false)
+            hasNext.set(tootListResponse.isNotEmpty())
+        }
     }
 
     private suspend fun reloadTootList() = withContext(Dispatchers.Main) {
