5f5d39f4763197f9f2db5567885dead70b921443
LAPTOP-OB2G7TH9\Owner <bkg98970@gmail.com>
2021-10-02 10:38:56 +0900

    P187まで進めた エラーになる

--- app/src/main/java/dev/keiji/sample/myapplication/MainActivity.kt
+++ app/src/main/java/dev/keiji/sample/myapplication/MainActivity.kt
@@ -4,13 +4,38 @@ package dev.keiji.sample.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dev.keiji.sample.myapplication.ui.TootListFragment
+import androidx.databinding.DataBindingUtil
+import dev.keiji.sample.mastodonclient.R
+import dev.keiji.sample.mastodonclient.databinding.ActivityMainBinding
+import dev.keiji.sample.myapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        if(savedInstanceState == null) {
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            val fragment = when (it.itemId) {
+                R.id.menu_home -> TootListFragment()
+                R.id.menu_public -> TootListFragment()
+                else -> null
+            }
+            fragment ?: return@setOnNavigationItemSelectedListener false
+
+            supportFragmentManager.beginTransaction()
+                .replace(
+                    R.id.fragment_container,
+                    fragment,
+                    TootListFragment.TAG
+                )
+                .commit()
+
+            return@setOnNavigationItemSelectedListener true
+        }
+
+        if (savedInstanceState == null) {
             val fragment = TootListFragment()
             supportFragmentManager.beginTransaction()
                 .add(
@@ -20,7 +45,7 @@ class MainActivity : AppCompatActivity() {
                 )
                 .commit()
         }
-        }
     }
+}
 
 
